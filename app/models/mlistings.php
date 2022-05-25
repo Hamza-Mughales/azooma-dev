e494a647d2c69799f2b360196a6a828fef295202
arafat_thabet <arafat.733011506@gmail.com>
2022-05-25 03:01:48 +0300

    fix error

--- app/models/mlistings.php
+++ app/models/mlistings.php
@@ -30,7 +30,8 @@ class MListings extends Eloquent{
 		return DB::select(DB::raw($cuisineq),array('cityid'=>$city));
 	}
 	public function getCustomCuisines($cityid=0,$cuisinename=""){
-		$mastercuisines=DB::connection('new-sufrati')->select('SELECT DISTINCT id,name,name_ar FROM master_cuisine WHERE name="'.$cuisinename.'"');
+	
+		$mastercuisines=DB::select('SELECT DISTINCT id,name,name_ar FROM master_cuisine WHERE name="'.$cuisinename.'"');
 		$mastercuisiness = $mastercuisines[0];
 
         $maincuisines=DB::connection('new-sufrati')->select('SELECT DISTINCT cu.cuisine_ID,cu.seo_url,cu.cuisine_Name,cu.cuisine_Name_ar,COUNT(DISTINCT rc.rest_ID) as total FROM cuisine_list cu JOIN rest_branches rb ON rb.city_ID='.$cityid.' AND rb.status=1 JOIN restaurant_info ri ON ri.rest_ID=rb.rest_fk_id AND ri.rest_Status=1 JOIN restaurant_cuisine rc ON rc.cuisine_ID=cu.cuisine_ID AND rc.rest_ID=rb.rest_fk_id WHERE cu.cuisine_Status=1 AND cu.master_id='.$mastercuisiness->id.' GROUP BY cu.cuisine_ID ORDER BY cu.cuisine_Name ASC');
