ef7237d2e7bea2584b00eaf2843e6721a03ed0c2
Hamza Mughales <hamzawemughales@gmail.com>
2022-05-25 21:20:30 +0300

    Code Formater >>

--- app/controllers/GalleryController.php
+++ app/controllers/GalleryController.php
@@ -1,166 +1,171 @@
 <?php
-class GalleryController extends BaseController {
-	public function __construct(){
-		$this->MGallery=new MGallery();
+class GalleryController extends BaseController
+{
+	public function __construct()
+	{
+		$this->MGallery = new MGallery();
 	}
 
-	public function index(){
-		$lang=Config::get('app.locale');
-		if($lang=="ar"){
-			$cityurl=Request::segment(2);
-		}else{
-			$cityurl=Request::segment(1);
+	public function index()
+	{
+		$lang = Config::get('app.locale');
+		if ($lang == "ar") {
+			$cityurl = Request::segment(2);
+		} else {
+			$cityurl = Request::segment(1);
 		}
-		$data['lang']=$lang;
-		$city=MGeneral::getCityURL($cityurl,true);
-		if(count($city)>0){
-			$cityname=($lang=="en")?stripcslashes($city->city_Name):stripcslashes($city->city_Name_ar);
-			$cityid=$city->city_ID;
-			$data['city']=$city;
-			$data['lang']=$lang;
-			$data['cityname']=$cityname;
-			$data['sufratiphotos']=$this->MGallery->getPhotos($cityid,0,3,0);
-			$data['videos']=$this->MGallery->getVideos(3,0);
-			$data['meta']=array(
-				'title'=>Lang::choice('messages.restaurants',1).' '.Lang::get('messages.photos').' '.Lang::get('messages.and').' '.Lang::get('messages.videos').' '.Lang::get('messages.inplace2',array('name'=>$cityname)),
-				'metadesc'=>Lang::get('metadesc.gallery',array('name'=>$cityname)),
-				'metakey'=>Lang::get('metakey.gallery',array('name'=>$cityname))
+		$data['lang'] = $lang;
+		$city = MGeneral::getCityURL($cityurl, true);
+		if (count($city) > 0) {
+			$cityname = ($lang == "en") ? stripcslashes($city->city_Name) : stripcslashes($city->city_Name_ar);
+			$cityid = $city->city_ID;
+			$data['city'] = $city;
+			$data['lang'] = $lang;
+			$data['cityname'] = $cityname;
+			$data['sufratiphotos'] = $this->MGallery->getPhotos($cityid, 0, 3, 0);
+			$data['videos'] = $this->MGallery->getVideos(3, 0);
+			$data['meta'] = array(
+				'title' => Lang::choice('messages.restaurants', 1) . ' ' . Lang::get('messages.photos') . ' ' . Lang::get('messages.and') . ' ' . Lang::get('messages.videos') . ' ' . Lang::get('messages.inplace2', array('name' => $cityname)),
+				'metadesc' => Lang::get('metadesc.gallery', array('name' => $cityname)),
+				'metakey' => Lang::get('metakey.gallery', array('name' => $cityname))
 			);
-			return View::make('gallery',$data);
-		}else{
+			return View::make('gallery', $data);
+		} else {
 			App::abort(404);
 		}
-
 	}
 
-	public function photos(){
-		
-		$lang=Config::get('app.locale');
-		if($lang=="ar"){
-			$cityurl=Request::segment(2);
-		}else{
-			$cityurl=Request::segment(1);
+	public function photos()
+	{
+
+		$lang = Config::get('app.locale');
+		if ($lang == "ar") {
+			$cityurl = Request::segment(2);
+		} else {
+			$cityurl = Request::segment(1);
 		}
-	
-		$sort="latest";
-		$data['lang']=$lang;
-		$limit=9;
-		$offset=0;
-		$city=MGeneral::getCityURL($cityurl,true);
-		
-		if(count($city)>0){
-			$cityname=($lang=="en")?stripcslashes($city->city_Name):stripcslashes($city->city_Name_ar);
-			$cityid=$city->city_ID;
-			$data['city']=$city;
-			$data['cityname']=$cityname;
-			$page=1;
-			if(Input::has('sort')){
-				$sort=Input::get('sort');
+
+		$sort = "latest";
+		$data['lang'] = $lang;
+		$limit = 9;
+		$offset = 0;
+		$city = MGeneral::getCityURL($cityurl, true);
+
+		if (count($city) > 0) {
+			$cityname = ($lang == "en") ? stripcslashes($city->city_Name) : stripcslashes($city->city_Name_ar);
+			$cityid = $city->city_ID;
+			$data['city'] = $city;
+			$data['cityname'] = $cityname;
+			$page = 1;
+			if (Input::has('sort')) {
+				$sort = Input::get('sort');
 			}
 			// if(Input::has('limit')){
 			// 	$limit=Input::get('limit');
 			// }
-			if(Input::has('page')){
-				$offset=$limit*(Input::get('page')-1);
-				$page=Input::get('page');
+			if (Input::has('page')) {
+				$offset = $limit * (Input::get('page') - 1);
+				$page = Input::get('page');
 			}
-			$data['photos']=$this->MGallery->getPhotos($cityid,0,$limit,$offset,FALSE,$sort);
-			$total=$this->MGallery->getPhotos($cityid,0,$limit,$offset,TRUE,$sort);
-		
-			$data['sort']=$sort;
-			$data['paginator']=Paginator::make($data['photos'],$total,$limit);
-			$data['originallink']=Azooma::URL($city->seo_url.'/photos');
-			$data['total']=$total;
-			if($page>1){
-				$prev=$page-1;
-				$data['prev']=Azooma::URL($city->seo_url.'/photos?page='.$prev);
+			$data['photos'] = $this->MGallery->getPhotos($cityid, 0, $limit, $offset, FALSE, $sort);
+			$total = $this->MGallery->getPhotos($cityid, 0, $limit, $offset, TRUE, $sort);
+
+			$data['sort'] = $sort;
+			$data['paginator'] = Paginator::make($data['photos'], $total, $limit);
+			$data['originallink'] = Azooma::URL($city->seo_url . '/photos');
+			$data['total'] = $total;
+			if ($page > 1) {
+				$prev = $page - 1;
+				$data['prev'] = Azooma::URL($city->seo_url . '/photos?page=' . $prev);
 			}
-			if(($offset+$limit)<$total){
-				$next=$page+1;
-				$data['next']=Azooma::URL($city->seo_url.'/photos?page='.$next);	
+			if (($offset + $limit) < $total) {
+				$next = $page + 1;
+				$data['next'] = Azooma::URL($city->seo_url . '/photos?page=' . $next);
 			}
-			$data['var']=array(
-				'limit'=>$limit,
-				'sort'=>$sort,
+			$data['var'] = array(
+				'limit' => $limit,
+				'sort' => $sort,
 			);
-			
-			$data['meta']=array(
-				'title'=>Lang::choice('messages.restaurants',1).' '.Lang::get('messages.photos').' '.Lang::get('messages.inplace2',array('name'=>$cityname)),
-				'metadesc'=>Lang::get('metadesc.photogallery',array('name'=>$cityname)),
-				'metakey'=>Lang::get('metakey.photogallery',array('name'=>$cityname))
+
+			$data['meta'] = array(
+				'title' => Lang::choice('messages.restaurants', 1) . ' ' . Lang::get('messages.photos') . ' ' . Lang::get('messages.inplace2', array('name' => $cityname)),
+				'metadesc' => Lang::get('metadesc.photogallery', array('name' => $cityname)),
+				'metakey' => Lang::get('metakey.photogallery', array('name' => $cityname))
 			);
-		
-			return view('photos',$data);
-		}else{
+
+			return view('photos', $data);
+		} else {
 			App::abort(404);
 		}
 	}
 
 
-	public function videos(){
-		$limit=12;
-		$offset=0;
-		$lang=Config::get('app.locale');
-		$data['lang']=$lang;
-		if(Input::has('limit')){
-			$limit=Input::get('limit');
+	public function videos()
+	{
+		$limit = 12;
+		$offset = 0;
+		$lang = Config::get('app.locale');
+		$data['lang'] = $lang;
+		if (Input::has('limit')) {
+			$limit = Input::get('limit');
 		}
-		if($lang=="ar"){
-			$cityurl=Request::segment(2);
-		}else{
-			$cityurl=Request::segment(1);
+		if ($lang == "ar") {
+			$cityurl = Request::segment(2);
+		} else {
+			$cityurl = Request::segment(1);
 		}
-		$city=MGeneral::getCityURL($cityurl,true);
-		if(count($city)>0){
-			$cityname=($lang=="en")?stripcslashes($city->city_Name):stripcslashes($city->city_Name_ar);
-			$cityid=$city->city_ID;
-			$data['city']=$city;
-			$data['cityname']=$cityname;
+		$city = MGeneral::getCityURL($cityurl, true);
+		if (count($city) > 0) {
+			$cityname = ($lang == "en") ? stripcslashes($city->city_Name) : stripcslashes($city->city_Name_ar);
+			$cityid = $city->city_ID;
+			$data['city'] = $city;
+			$data['cityname'] = $cityname;
 		}
-		$page=1;
-		if(Input::has('page')){
-			$offset=$limit*(Input::get('page')-1);
-			$page=Input::get('page');
+		$page = 1;
+		if (Input::has('page')) {
+			$offset = $limit * (Input::get('page') - 1);
+			$page = Input::get('page');
 		}
-		$data['videos']=$this->MGallery->getVideos($limit,$offset);
-		$total=$this->MGallery->getTotalVideos();
-		$data['total']=$total;
-		$data['paginator']=Paginator::make($data['videos'],$total,$limit);
-		$data['paginatevar']=array(
-			'limit'=>$limit
+		$data['videos'] = $this->MGallery->getVideos($limit, $offset);
+		$total = $this->MGallery->getTotalVideos();
+		$data['total'] = $total;
+		$data['paginator'] = Paginator::make($data['videos'], $total, $limit);
+		$data['paginatevar'] = array(
+			'limit' => $limit
 		);
-		$data['originallink']=Azooma::URL('videos');
-		if($page>1){
-			$prev=$page-1;
-			$data['prev']=Azooma::URL('videos?page='.$prev);
+		$data['originallink'] = Azooma::URL('videos');
+		if ($page > 1) {
+			$prev = $page - 1;
+			$data['prev'] = Azooma::URL('videos?page=' . $prev);
 		}
-		if(($offset+$limit)<$total){
-			$next=$page+1;
-			$data['next']=Azooma::URL('videos?page='.$next);	
+		if (($offset + $limit) < $total) {
+			$next = $page + 1;
+			$data['next'] = Azooma::URL('videos?page=' . $next);
 		}
-		$data['meta']=array(
-			'title'=>Lang::choice('messages.restaurants',1).' '.Lang::get('messages.videos'),
-			'metadesc'=>Lang::get('metadesc.videogallery'),
-			'metakey'=>Lang::get('metakey.videogallery')
+		$data['meta'] = array(
+			'title' => Lang::choice('messages.restaurants', 1) . ' ' . Lang::get('messages.videos'),
+			'metadesc' => Lang::get('metadesc.videogallery'),
+			'metakey' => Lang::get('metakey.videogallery')
 		);
-		return View::make('videos',$data);
+		return View::make('videos', $data);
 	}
 
-	public function video($id=0){
-		$lang=Config::get('app.locale');
-		$data['lang']=$lang;
-		$data['video']=$this->MGallery->getVideo($id);
-		$data['videoname']=($lang=="en")?ucfirst(stripcslashes($data['video'][0]->name_en)):ucfirst(stripcslashes($data['video'][0]->name_ar));
-		if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' ){
-			$t['html']=stripcslashes(View::make('ajax.video',$data));	
+	public function video($id = 0)
+	{
+		$lang = Config::get('app.locale');
+		$data['lang'] = $lang;
+		$data['video'] = $this->MGallery->getVideo($id);
+		$data['videoname'] = ($lang == "en") ? ucfirst(stripcslashes($data['video'][0]->name_en)) : ucfirst(stripcslashes($data['video'][0]->name_ar));
+		if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+			$t['html'] = stripcslashes(View::make('ajax.video', $data));
 			return Response::json($t);
-		}else{
-			$data['videos']=$this->MGallery->getVideos(4,0,$id);
-			$data['meta']=array(
-				'title'=>$data['videoname'],
-				'metadesc'=>$data['videoname']
+		} else {
+			$data['videos'] = $this->MGallery->getVideos(4, 0, $id);
+			$data['meta'] = array(
+				'title' => $data['videoname'],
+				'metadesc' => $data['videoname']
 			);
-			return View::make('video',$data);
+			return View::make('video', $data);
 		}
 	}
 }
--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
-class HomeController extends BaseController {
+class HomeController extends BaseController
+{
 	/*
 	|--------------------------------------------------------------------------
 	| Default Home Controller
@@ -13,88 +14,89 @@ class HomeController extends BaseController {
 	|	Route::get('/', 'HomeController@showWelcome');
 	|
 	*/
-	public function __construct(){
-			$this->MListings = new MListings();
+	public function __construct()
+	{
+		$this->MListings = new MListings();
 	}
 
 
-	public function index()	{
-		$lang=Config::get('app.locale');
-		$langhelperstring="";
-		if($lang=="ar"){
-			$langhelperstring='ar/';
+	public function index()
+	{
+		$lang = Config::get('app.locale');
+		$langhelperstring = "";
+		if ($lang == "ar") {
+			$langhelperstring = 'ar/';
 		}
-		
-		$data['lang']=$lang;
-		$data['langhelperstring']=$langhelperstring;
-		
-		$data['landing']=true;
-		
+
+		$data['lang'] = $lang;
+		$data['langhelperstring'] = $langhelperstring;
+
+		$data['landing'] = true;
+
 		$data['featured'] = ArtWork::where('art_work_name', 'Home Page Artwork')->where('active', 1)->whereIn('country', adminCountry())->get()->toArray();
 		// dd($data['featured']);
 
-		$countries=DB::table('aaa_country')->select('id','flag','name','nameAr')->get();
-		$countriesname="";
-		$i=0;
-		$tcountries=array();
+		$countries = DB::table('aaa_country')->select('id', 'flag', 'name', 'nameAr')->get();
+		$countriesname = "";
+		$i = 0;
+		$tcountries = array();
 		foreach ($countries as $country) {
 			$i++;
-			$countriesname.=($lang=="en")?stripcslashes($country->name):stripcslashes($country->nameAr);
-			if($i<count($countries)){
-				$countriesname.=', ';
+			$countriesname .= ($lang == "en") ? stripcslashes($country->name) : stripcslashes($country->nameAr);
+			if ($i < count($countries)) {
+				$countriesname .= ', ';
 			}
-			$cities=DB::table('city_list')->select('city_Name','city_Name_ar','seo_url','city_thumbnail','city_ID')->where('city_status',1)->where('country',$country->id)->orderBy('city_Name','asc')->get();
-			$tcountries[$i]=$country;
-			$tcountries[$i]->cities=$cities;
+			$cities = DB::table('city_list')->select('city_Name', 'city_Name_ar', 'seo_url', 'city_thumbnail', 'city_ID')->where('city_status', 1)->where('country', $country->id)->orderBy('city_Name', 'asc')->get();
+			$tcountries[$i] = $country;
+			$tcountries[$i]->cities = $cities;
 		}
-		$data['countries']=$tcountries;
+		$data['countries'] = $tcountries;
 
 		$mycities = "";
-        $city = "";
-        $country = "";
-        if(Session::has('sfcity')){
-            $city=DB::table('city_list')->select('city_Name','city_Name_ar','seo_url','city_thumbnail','city_ID')->where('city_ID',Session::get('sfcity'))->where('city_status',1)->first();
-            $countryID=DB::table('city_list')->select('country')->where('seo_url',$city->seo_url)->first();
-            $country=DB::table('aaa_country')->select('id','flag','name','nameAr')->where('id',$countryID->country)->get();
-            $mycities=DB::table('city_list')->select('city_Name','city_Name_ar','seo_url','city_thumbnail')->where('city_status',1)->where('country',$countryID->country)->orderBy('city_Name','asc')->get();
-            $city=$city=MGeneral::getCityURL($city->seo_url);
-        }else{
-            $ip = $_SERVER['REMOTE_ADDR'];
-            $details = "";
-			if($ip == 0){
+		$city = "";
+		$country = "";
+		if (Session::has('sfcity')) {
+			$city = DB::table('city_list')->select('city_Name', 'city_Name_ar', 'seo_url', 'city_thumbnail', 'city_ID')->where('city_ID', Session::get('sfcity'))->where('city_status', 1)->first();
+			$countryID = DB::table('city_list')->select('country')->where('seo_url', $city->seo_url)->first();
+			$country = DB::table('aaa_country')->select('id', 'flag', 'name', 'nameAr')->where('id', $countryID->country)->get();
+			$mycities = DB::table('city_list')->select('city_Name', 'city_Name_ar', 'seo_url', 'city_thumbnail')->where('city_status', 1)->where('country', $countryID->country)->orderBy('city_Name', 'asc')->get();
+			$city = $city = MGeneral::getCityURL($city->seo_url);
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+			$details = "";
+			if ($ip == 0) {
 				$details = json_decode(file_get_contents("http://ip-api.com/json/"));
-			}else{
-				$details = json_decode(file_get_contents("http://ip-api.com/json/".$ip));
+			} else {
+				$details = json_decode(file_get_contents("http://ip-api.com/json/" . $ip));
 			}
-            $currentCountryIP = $details->country;
-            $Getcityname = $details->city;
-           
-            $country=DB::table('aaa_country')->select('id','flag','name','nameAr')->where('name',$currentCountryIP)->first();
-			if(count($country) == 0){
-				$country=DB::table('aaa_country')->select('id','flag','name','nameAr')->first();
-			   }
-            $mycities=DB::table('city_list')->select('city_Name','city_Name_ar','seo_url','city_thumbnail')->where('city_status',1)->where('country',$country->id)->orderBy('city_Name','asc')->get();
-            $city=DB::table('city_list')->select('city_ID','city_Name','city_Name_ar','seo_url','city_thumbnail')->where('city_Name',$Getcityname)->where('city_status',1)->first();
-            if(count($city) > 0){
-            }else{
-                $city=DB::table('city_list')->select('city_ID','city_Name','city_Name_ar','seo_url','city_thumbnail')->where('country',$country->id)->where('city_status',1)->first();
-            }
-   
-            Config::set('session.lifetime',365*12*3600);
-            Session::put('sfcity',$city->city_ID);
-        }
-        $city=$city=MGeneral::getCityURL($city->seo_url);
-        $data['city']=$city;
-        $data['country']=$country;
-        $data['mycities']=$mycities;
-		$data['meta']=array(
-			'metadesc'=>Lang::get('metadesc.landing_page',array('name'=>$countriesname)),
-			'metakey'=>Lang::get('metakey.landing_page'),
-			
+			$currentCountryIP = $details->country;
+			$Getcityname = $details->city;
+
+			$country = DB::table('aaa_country')->select('id', 'flag', 'name', 'nameAr')->where('name', $currentCountryIP)->first();
+			if (count($country) == 0) {
+				$country = DB::table('aaa_country')->select('id', 'flag', 'name', 'nameAr')->first();
+			}
+			$mycities = DB::table('city_list')->select('city_Name', 'city_Name_ar', 'seo_url', 'city_thumbnail')->where('city_status', 1)->where('country', $country->id)->orderBy('city_Name', 'asc')->get();
+			$city = DB::table('city_list')->select('city_ID', 'city_Name', 'city_Name_ar', 'seo_url', 'city_thumbnail')->where('city_Name', $Getcityname)->where('city_status', 1)->first();
+			if (count($city) > 0) {
+			} else {
+				$city = DB::table('city_list')->select('city_ID', 'city_Name', 'city_Name_ar', 'seo_url', 'city_thumbnail')->where('country', $country->id)->where('city_status', 1)->first();
+			}
+
+			Config::set('session.lifetime', 365 * 12 * 3600);
+			Session::put('sfcity', $city->city_ID);
+		}
+		$city = $city = MGeneral::getCityURL($city->seo_url);
+		$data['city'] = $city;
+		$data['country'] = $country;
+		$data['mycities'] = $mycities;
+		$data['meta'] = array(
+			'metadesc' => Lang::get('metadesc.landing_page', array('name' => $countriesname)),
+			'metakey' => Lang::get('metakey.landing_page'),
+
 		);
 		// dd('home', $lang, $data );
 
-		return view('home',$data);
+		return view('home', $data);
 	}
-
 }
